3938ff363da89a1b2369ef724b631ed05068eb17
whoward2929 <Whoward939@gmail.com>
2021-10-24 22:38:43 -0400

    service logic

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -47,21 +47,41 @@ self.addEventListener('activate', function(e) {
   );
 });
 
-self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      //intercept the fetch request, see what its trying to fetch, then if the files being searched for can be found in the cache, return request, otherwise proceed with the fetch.
-      return request || fetch(e.request)
-      /*
-      The line above does the same as this:
-      if (request) {
-        return request
-      } 
-      else {
-        return fetch(e.request)
-      }
-      */
+// Respond with cached resources
+self.addEventListener("fetch", function(event) {
+  // cache all get requests to /api routes
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
+            }
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(event.request);
+          });
+      }).catch(err => console.log(err))
+    );
+
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          // return the cached home page for all requests for html pages
+          return caches.match("/");
+        }
+      });
+      
     })
-  )
-}) 
+
+  );});
